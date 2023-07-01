ae999f4e677255b3def62a0b1efd89a1929b0b57
wojtko <j.w2001@onet.pl>
2023-07-01 10:58:44 +0200

    Add home page logo. Start creating home page.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,15 @@ function Home() {
     <section id="home">
       <div className="container">
         <div className="home-main">
+          <div className="home-text">
+            <h1>Your best food.</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua
+            </p>
+          </div>
+
+          <div className="home-image"></div>
         </div>
       </div>
     </section>
