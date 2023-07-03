5b600883c94f55e84aee1f34f20193b19a97934f
wojtko <j.w2001@onet.pl>
2023-07-03 23:55:41 +0200

    Start creating footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,25 @@ function Footer() {
     <footer className="footer">
       <div className="container">
         <div className="container-items">
-          <span className="navbar-logo-footer">
-            <span className="navbar-logo-first">Thai</span>
-            <span className="navbar-logo-second">Food</span>
-          </span>
+          <div className="logo">
+            <span className="navbar-logo-footer">
+              <span className="navbar-logo-first">Thai</span>
+              <span className="navbar-logo-second">Food</span>
+            </span>
+          </div>
+          <div className="footer-menu">
+            <ul>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Menu</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </footer>
