3bdd9f40e45bea27802c13ea036b92d3520e43fc
wojtko <j.w2001@onet.pl>
2023-07-20 23:59:04 +0200

    Add fade effect on the dishes

--- src/components/Menu-item.jsx
+++ src/components/Menu-item.jsx
@@ -1,28 +1,31 @@
 import React from "react";
+import Fade from 'react-reveal/Fade';
 
 function MenuItem({ dishes }) {
   return (
     <div className="menu-items">
       {dishes.map((dish) => {
         return (
-          <div className="menu-item-card" key={dish.id}>
-            <div className="menu-item-image">
-              <img src={dish.img} alt="food"></img>
-            </div>
+          <Fade bottom>
+            <div className="menu-item-card" key={dish.id}>
+              <div className="menu-item-image">
+                <img src={dish.img} alt="food"></img>
+              </div>
 
-            <div className="menu-item-text">
-              <h3>{dish.name}</h3>
-              <span>{dish.description}</span>
-              <div className="menu-item-text-cart">
-                <span>
-                  <b>{dish.amount}</b>
-                </span>
-                <div className="menu-item-text-cart-i">
-                  <i class="fa-solid fa-bag-shopping"></i>
+              <div className="menu-item-text">
+                <h3>{dish.name}</h3>
+                <span>{dish.description}</span>
+                <div className="menu-item-text-cart">
+                  <span>
+                    <b>{dish.amount}</b>
+                  </span>
+                  <div className="menu-item-text-cart-i">
+                    <i class="fa-solid fa-bag-shopping"></i>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          </Fade>
         );
       })}
     </div>
