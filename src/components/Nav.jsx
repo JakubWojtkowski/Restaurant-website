4c89f4f391bd075717be3f153f1f3697c0612763
wojtko <j.w2001@onet.pl>
2023-07-21 23:59:07 +0200

    Improving dropdown menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -22,7 +22,52 @@ function Nav() {
               <a href="#">Contact</a>
             </li>
           </ul>
+          <button
+            className="toggle-btn"
+            onClick={(e) => {
+              const dropDownMenu = document.querySelector(".dropdown-menu");
+              dropDownMenu.classList.toggle("open");
+
+              const isOpen = dropDownMenu.classList.contains("open");
+
+              isOpen
+                ? setIconClass("fa-solid fa-xmark")
+                : setIconClass("fa-solid fa-bars-staggered");
+            }}
+          >
+            <i class={iconClass}></i>
+          </button>
+
+          <div className="dropdown-menu">
+          <ul
+            className="dropdown-menu-links"
+            onClick={() => {
+              const dropDownMenu = document.querySelector(".dropdown-menu");
+              dropDownMenu.classList.toggle("open");
+              dropDownMenu.classList.toggle("closed");
+
+              const isClosed = dropDownMenu.classList.contains("closed");
+
+              isClosed
+                ? setIconClass("fa-solid fa-bars-staggered")
+                : setIconClass("fa-solid fa-xmark");
+              dropDownMenu.classList.toggle("closed");
+            }}
+          >
+            <li>
+              <a href="#home">About</a>
+            </li>
+            <li>
+              <a href="#about">Menu</a>
+            </li>
+            <li>
+              <a href="#skills">Contact</a>
+            </li>
+          
+          </ul>
+        </div>
         </nav>
+
         <div className="navbar-socials">
           <div className="navbar-socials-item">
             <a href="#">
